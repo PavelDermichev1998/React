8dad112b9f96ef8ee62b585ca4965fb99a16a9da
PavelDermichev1998 <pdermichev@gmail.com>
2022-04-22 15:02:01 +0300

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,63 @@
-import React from 'react';
+import React, {useState, useReducer, useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const changeCounter = (state: number, action: any): number => {
+    switch (action.type) {
+        case "INC_VALUE":
+            return state + 1
+        case "RESET":
+            return 0
+        case "DEC_VALUE":
+            return state - 1
+        default:
+            return state
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function Counter() {
+    const [value, setValue] = useReducer(changeCounter, 0)
+    const [isCounter, setIsCounter] = useState(true)
+
+    const commonStyles: React.CSSProperties = {
+        border: "1px solid black",
+        margin: "100px auto",
+        width: "300px",
+        height: "150px",
+        textAlign: "center",
+    }
+    const btnStyles: React.CSSProperties = {
+        color: "white",
+        fontWeight: "bold",
+        backgroundColor: "darkgray",
+        borderRadius: "3px",
+        minWidth: "40px"
+    }
+
+    return (
+        <div style={commonStyles}>{isCounter
+            ?
+            <div>
+                <div style={{marginBottom: "20px"}}>
+                    <h2>{value}</h2>
+                    <button style={{...btnStyles, backgroundColor: "red"}}
+                            onClick={() => setIsCounter(false)}>
+                        OFF</button>
+                </div>
+                <button style={btnStyles} onClick={() => setValue({type: "INC_VALUE"})}>+</button>
+                <button style={btnStyles} onClick={() => setValue({type: "RESET"})}>0</button>
+                <button style={btnStyles} onClick={() => setValue({type: "DEC_VALUE"})}>-</button>
+            </div>
+            :
+            <div style={{textAlign: "center"}}><h2>Counter not working</h2>
+                <button style={{...btnStyles, backgroundColor: "green"}} onClick={() => setIsCounter(true)}>ON</button>
+            </div>} </div>)
+}
+
+ReactDOM.render(<Counter/>, document.getElementById('root'));
+// Что надо написать вместо XXX и YYY, чтобы код работал? Напишите через пробел.
+
+
+
+
+
